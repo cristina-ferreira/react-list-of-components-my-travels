5273239929a428661b0a147fbf38a48867e35377
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-09-24 12:28:34 +0100

    Updates

---
+++ precache-manifest.3a216682787155b46d904b7d4997b02b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd92c27352c0f57676a9a271beca17a",
+    "url": "/react-list-of-components-my-travels/index.html"
+  },
+  {
+    "revision": "8bc52a0050478b650c59",
+    "url": "/react-list-of-components-my-travels/static/css/main.5c2cdc4f.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react-list-of-components-my-travels/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "8bc52a0050478b650c59",
+    "url": "/react-list-of-components-my-travels/static/js/main.053f5a40.chunk.js"
+  },
+  {
+    "revision": "66baf18476734f4ba3bb",
+    "url": "/react-list-of-components-my-travels/static/js/runtime~main.7f1bbb33.js"
+  }
+]);
